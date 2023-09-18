c7c8315dda454ef591919fcf0eb216dff424df67
Alain Royer <Alain.Royer@mttservicecenter.com>
2023-09-18 15:57:02 -0400

    wip

--- Config/digini_cfg.h
+++ Config/digini_cfg.h
@@ -49,7 +49,7 @@
 #define DIGINI_USE_CMD_LINE                             DEF_ENABLED
 #define DIGINI_USE_VT100_MENU                           DEF_ENABLED
 
-#define DIGINI_USE_ETHERNET                             DEF_ENABLED
+#define DIGINI_USE_ETHERNET                             DEF_DISABLED
 
 //-------------------------------------------------------------------------------------------------
 // High level Peripheral
