29c58e23f01a7704eec8d4e54775413216997761
Alain Royer <Alain.Royer@mttservicecenter.com>
2024-08-01 08:14:54 -0400

    change config to sync with Digini

--- Config/spi_cfg.h
+++ Config/spi_cfg.h
@@ -37,5 +37,7 @@
 #define SPI_DRIVER_SUPPORT_SPI5_CFG         DEF_DISABLED
 #define SPI_DRIVER_SUPPORT_SPI6_CFG         DEF_DISABLED
 
+#define SPI_DRIVER_SUPPORT_DMA_CFG          DEF_ENABLED
+
 //-------------------------------------------------------------------------------------------------
 
--- Config/spi_var.h
+++ Config/spi_var.h
@@ -32,25 +32,34 @@
 // Define(s)
 //-------------------------------------------------------------------------------------------------
 
-#define SPI_STANDARD_CONFIGURATION_RX ( DMA_MODE_NORMAL             | \
-                                        DMA_PERIPHERAL_TO_MEMORY    | \
-                                        DMA_PERIPHERAL_NO_INCREMENT | \
-                                        DMA_MEMORY_INCREMENT        | \
-                                        DMA_PERIPHERAL_SIZE_8_BITS  | \
-                                        DMA_MEMORY_SIZE_8_BITS      | \
-                                        DMA_PERIPHERAL_BURST_SINGLE | \
-                                        DMA_MEMORY_BURST_SINGLE     | \
-                                        DMA_PRIORITY_MEDIUM)
+#define SPI_STANDARD_CONFIG               ( SPI_MODE_MASTER       | \
+                                            SPI_DATA_WIDTH_8_BIT  | \
+                                            SPI_POLARITY_LOW      | \
+                                            SPI_PHASE_1_EDGE      | \
+                                            SPI_NSS_SOFT          | \
+                                            SPI_MSB_FIRST         | \
+                                            SPI_FULL_DUPLEX)
 
-#define SPI_STANDARD_CONFIGURATION_TX ( DMA_MODE_NORMAL             | \
-                                        DMA_MEMORY_TO_PERIPHERAL    | \
-                                        DMA_PERIPHERAL_NO_INCREMENT | \
-                                        DMA_MEMORY_INCREMENT        | \
-                                        DMA_PERIPHERAL_SIZE_8_BITS  | \
-                                        DMA_MEMORY_SIZE_8_BITS      | \
-                                        DMA_PERIPHERAL_BURST_SINGLE | \
-                                        DMA_MEMORY_BURST_SINGLE     | \
-                                        DMA_PRIORITY_MEDIUM)
+
+#define SPI_DMA_STANDARD_CONFIG_RX        ( DMA_MODE_NORMAL             | \
+                                            DMA_PERIPHERAL_TO_MEMORY    | \
+                                            DMA_PERIPHERAL_NO_INCREMENT | \
+                                            DMA_MEMORY_INCREMENT        | \
+                                            DMA_PERIPHERAL_SIZE_8_BITS  | \
+                                            DMA_MEMORY_SIZE_8_BITS      | \
+                                            DMA_PERIPHERAL_BURST_SINGLE | \
+                                            DMA_MEMORY_BURST_SINGLE     | \
+                                            DMA_PRIORITY_MEDIUM)
+
+#define SPI_DMA_STANDARD_CONFIG_TX        ( DMA_MODE_NORMAL             | \
+                                            DMA_MEMORY_TO_PERIPHERAL    | \
+                                            DMA_PERIPHERAL_NO_INCREMENT | \
+                                            DMA_MEMORY_INCREMENT        | \
+                                            DMA_PERIPHERAL_SIZE_8_BITS  | \
+                                            DMA_MEMORY_SIZE_8_BITS      | \
+                                            DMA_PERIPHERAL_BURST_SINGLE | \
+                                            DMA_MEMORY_BURST_SINGLE     | \
+                                            DMA_PRIORITY_MEDIUM)
 
 //-------------------------------------------------------------------------------------------------
 // Constant(s)
@@ -63,56 +72,61 @@ SPI_Info_t SPI_Info[NB_OF_SPI_DRIVER] =
   #if (SPI_DRIVER_SUPPORT_SPI1_CFG == DEF_ENABLED)
 	{
         SPI1,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI1_CLK,                // CLK
         IO_SPI1_MOSI,               // MOSI
         IO_SPI1_MISO,               // MISO
-        SPI_SPEED_FCLK_DIV64,
-        SPI_FULL_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
+        IO_NOT_DEFINED,             // NSS  N/U
         SPI1_IRQn,                  // IRQn_Channel
 
+      #if (SPI_DRIVER_SUPPORT_DMA_CFG == ENABLED)
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_3, // Configuration + DMA_Channel RX
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_3,    // Configuration + DMA_Channel RX
             DMA_LIFCR_CTCIF0,                              // RX_IT_Flag
             DMA2_Stream0,                                  // RX_DMA_TypeDef
             DMA2_Stream0_IRQn,                             // RX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_3, // Configuration + DMA_Channel RX
+         // SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_3,    // Configuration + DMA_Channel RX
          // DMA_LIFCR_CTCIF2,                              // RX_IT_Flag
          // DMA1_Stream2,                                  // RX_DMA_TypeDef
          // DMA1_Stream2_IRQn,                             // RX_IRQn
         },
-        
+
         // DMA TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_3, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_3,    // Configuration + DMA_Channel TX
             DMA_LIFCR_CTCIF3,                              // TX_IT_Flag
             DMA2_Stream3,                                  // TX_DMA_TypeDef
             DMA2_Stream3_IRQn,                             // TX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_3, // Configuration + DMA_Channel TX
+         // SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_3,    // Configuration + DMA_Channel TX
          // DMA_HIFCR_CTCIF5,                              // TX_IT_Flag
          // DMA2_Stream5,                                  // TX_DMA_TypeDef
          // DMA2_Stream5_IRQn,                             // TX_IRQn
         },
+      #endif
 	},
   #endif
 
   #if (SPI_DRIVER_SUPPORT_SPI2_CFG == DEF_ENABLED)
 	{
 		SPI2,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI2_CLK,                // CLK
         IO_SPI2_MOSI,               // MOSI
         IO_NOT_DEFINED,             // MISO
-		SPI_SPEED_FCLK_DIV64,
-		SPI_HALF_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
+        IO_NOT_DEFINED,             // NSS  N/U
 		SPI2_IRQn,                  // IRQn_Channel
 
+      #if (SPI_DRIVER_SUPPORT_DMA_CFG == ENABLED)
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_0, // Configuration + DMA_Channel RX
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_0,    // Configuration + DMA_Channel RX
             DMA_LIFCR_CTCIF3,                              // RX_IT_Flag
             DMA1_Stream3,                                  // RX_DMA_TypeDef
             DMA1_Stream3_IRQn,                             // RX_IRQn
@@ -120,33 +134,36 @@ SPI_Info_t SPI_Info[NB_OF_SPI_DRIVER] =
 
         // DMA_TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_0, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_0,    // Configuration + DMA_Channel TX
             DMA_HIFCR_CTCIF4,                              // TX_IT_Flag
             DMA1_Stream4,                                  // TX_DMA_TypeDef
             DMA1_Stream4_IRQn,                             // TX_IRQn
         },
+      #endif
  	},
   #endif
 
   #if (SPI_DRIVER_SUPPORT_SPI3_CFG == DEF_ENABLED)
 	{
 		SPI3,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI3_CLK,                // CLK
         IO_SPI3_MOSI,               // MOSI
         IO_SPI3_MISO,               // MISO
-		SPI_SPEED_FCLK_DIV64,
-		SPI_FULL_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
+        IO_NOT_DEFINED,             // NSS  N/U
 		SPI3_IRQn,                  // IRQn_Channel
 
+      #if (SPI_DRIVER_SUPPORT_DMA_CFG == ENABLED)
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_0, // Configuration + DMA_Channel RX
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_0,    // Configuration + DMA_Channel RX
             DMA_LIFCR_CTCIF0,                              // RX_IT_Flag
             DMA1_Stream0,                                  // RX_DMA_TypeDef
             DMA1_Stream0_IRQn,                             // RX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_0, // Configuration + DMA_Channel RX
+         // SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_0,    // Configuration + DMA_Channel RX
          // DMA_LIFCR_CTCIF3,                              // RX_IT_Flag
          // DMA1_Stream3,                                  // RX_DMA_TypeDef
          // DMA1_Stream3_IRQn,                             // RX_IRQn
@@ -154,114 +171,134 @@ SPI_Info_t SPI_Info[NB_OF_SPI_DRIVER] =
 
         // DMA_TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_0, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_0,    // Configuration + DMA_Channel TX
             DMA_HIFCR_CTCIF5,                              // TX_IT_Flag
             DMA1_Stream5,                                  // TX_DMA_TypeDef
             DMA1_Stream5_IRQn,                             // TX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_0, // Configuration + DMA_Channel TX
+         // SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_0,    // Configuration + DMA_Channel TX
          // DMA_HIFCR_CTCIF7,                              // TX_IT_Flag
          // DMA1_Stream7,                                  // TX_DMA_TypeDef
          // DMA1_Stream7_IRQn,                             // TX_IRQn
         },
+      #endif
  	},
   #endif
 
   #if (SPI_DRIVER_SUPPORT_SPI4_CFG == DEF_ENABLED)
 	{
 		SPI4,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI4_CLK,                // CLK
         IO_SPI4_MOSI,               // MOSI
         IO_SPI4_MISO,               // MISO
-		SPI_SPEED_FCLK_DIV64,
-		SPI_FULL_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
+        IO_NOT_DEFINED,             // NSS  N/U
 		SPI4_IRQn,                  // IRQn_Channel
 
+      #if (SPI_DRIVER_SUPPORT_DMA_CFG == ENABLED)
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_5, // Configuration + DMA_Channel RX
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_5,    // Configuration + DMA_Channel RX
             DMA_LIFCR_CTCIF3,                              // RX_IT_Flag
             DMA1_Stream3,                                  // RX_DMA_TypeDef
             DMA1_Stream3_IRQn,                             // TX_IRQn
+            // Other choice
+         // SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_4,    // Configuration + DMA_Channel RX
+         // DMA_LIFCR_CTCIF0,                              // RX_IT_Flag
+         // DMA2_Stream0,                                  // RX_DMA_TypeDef
+         // DMA2_Stream0_IRQn,                             // RX_IRQn
         },
 
         // DMA_TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_5, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_5,    // Configuration + DMA_Channel TX
             DMA_HIFCR_CTCIF4,                              // TX_IT_Flag
             DMA1_Stream4,                                  // TX_DMA_TypeDef
             DMA1_Stream4_IRQn,                             // TX_IRQn
+
+            // Other choice
+         // SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_4,    // Configuration + DMA_Channel TX
+         // DMA_LIFCR_CTCIF1,                              // TX_IT_Flag
+         // DMA2_Stream1,                                  // TX_DMA_TypeDef
+         // DMA2_Stream1_IRQn,                             // TX_IRQn
         },
+      #endif
 	},
   #endif
 
   #if (SPI_DRIVER_SUPPORT_SPI5_CFG == DEF_ENABLED)
 	{
 		SPI5,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI5_CLK,                // CLK
         IO_SPI5_MOSI,               // MOSI
         IO_SPI5_MISO,               // MISO
-		SPI_SPEED_FCLK_DIV64,
-		SPI_FULL_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
-		SPI3_IRQn,                  // IRQn_Channel
+        IO_NOT_DEFINED,             // NSS  N/U
+		SPI5_IRQn,                  // IRQn_Channel
 
+      #if (SPI_DRIVER_SUPPORT_DMA_CFG == ENABLED)
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_2, // Configuration + DMA_Channel RX
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_2,    // Configuration + DMA_Channel RX
             DMA_LIFCR_CTCIF3,                              // RX_IT_Flag
-            DMA1_Stream3,                                  // RX_DMA_TypeDef
-            DMA1_Stream3_IRQn,                             // RX_IRQn
+            DMA2_Stream3,                                  // RX_DMA_TypeDef
+            DMA2_Stream3_IRQn,                             // RX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_7, // Configuration + DMA_Channel RX
-         // DMA_LIFCR_CTCIF5,                              // RX_IT_Flag
-         // DMA1_Stream5,                                  // RX_DMA_TypeDef
-         // DMA1_Stream5_IRQn,                             // RX_IRQn
+         // SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_7,    // Configuration + DMA_Channel RX
+         // DMA_HIFCR_CTCIF5,                              // RX_IT_Flag
+         // DMA2_Stream5,                                  // RX_DMA_TypeDef
+         // DMA2_Stream5_IRQn,                             // RX_IRQn
         },
 
         // DMA_TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_2, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_2,    // Configuration + DMA_Channel TX
             DMA_HIFCR_CTCIF4,                              // TX_IT_Flag
-            DMA1_Stream4,                                  // TX_DMA_TypeDef
-            DMA1_Stream4_IRQn,                             // TX_IRQn
+            DMA2_Stream4,                                  // TX_DMA_TypeDef
+            DMA2_Stream4_IRQn,                             // TX_IRQn
 
             // Other choice
-         // SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_7, // Configuration + DMA_Channel TX
+         // SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_7,    // Configuration + DMA_Channel TX
          // DMA_HIFCR_CTCIF6,                              // TX_IT_Flag
-         // DMA1_Stream6,                                  // TX_DMA_TypeDef
-         // DMA1_Stream6_IRQn,                             // TX_IRQn
+         // DMA2_Stream6,                                  // TX_DMA_TypeDef
+         // DMA2_Stream6_IRQn,                             // TX_IRQn
         },
+      #endif
 	},
   #endif
 
   #if (SPI_DRIVER_SUPPORT_SPI6_CFG == DEF_ENABLED)
 	{
 		SPI6,                       // SPIx
+        SPI_STANDARD_CONFIG,        // Configuration
+		24000000,                   // The DAC chip is 12 inche away from CPU on 790A, so reduced comm speed
         IO_SPI6_CLK,                // CLK
         IO_SPI6_MOSI,               // MOSI
         IO_SPI6_MISO,               // MISO
-		SPI_SPEED_FCLK_DIV64,
-		SPI_FULL_DUPLEX,            // SPI_FULL_DUPLEX or SPI_HALF_DUPLEX
+        IO_NOT_DEFINED,             // NSS  N/U
+		SPI6_IRQn,                  // IRQn_Channel
 
-		SPI3_IRQn,                  // IRQn_Channel
-
+      #endif
         // DMA_RX
         {
-            SPI_STANDARD_CONFIGURATION_RX | DMA_CHANNEL_1, // Configuration + DMA_Channel RX
-            DMA_LIFCR_CTCIF6,                              // RX_IT_Flag
-            DMA1_Stream6,                                  // RX_DMA_TypeDef
-            DMA1_Stream6_IRQn,                             // RX_IRQn
+            SPI_DMA_STANDARD_CONFIG_RX | DMA_CHANNEL_1,    // Configuration + DMA_Channel RX
+            DMA_HIFCR_CTCIF6,                              // RX_IT_Flag
+            DMA2_Stream6,                                  // RX_DMA_TypeDef
+            DMA2_Stream6_IRQn,                             // RX_IRQn
         },
 
         // DMA_TX
         {
-            SPI_STANDARD_CONFIGURATION_TX | DMA_CHANNEL_1, // Configuration + DMA_Channel TX
+            SPI_DMA_STANDARD_CONFIG_TX | DMA_CHANNEL_1,    // Configuration + DMA_Channel TX
             DMA_HIFCR_CTCIF5,                              // TX_IT_Flag
-            DMA1_Stream5,                                  // TX_DMA_TypeDef
-            DMA1_Stream5_IRQn,                             // TX_IRQn
+            DMA2_Stream5,                                  // TX_DMA_TypeDef
+            DMA2_Stream5_IRQn,                             // TX_IRQn
         },
+      #endif
 	},
   #endif
 };
